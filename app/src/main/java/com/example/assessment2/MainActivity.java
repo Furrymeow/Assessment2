b95eeb07997eedbd4a0c7c4c4be1cf9bc52eb92a
Willim <54743117+Furrymeow@users.noreply.github.com>
2019-09-08 18:28:53 +1000

    Initial commit

---
+++ app/src/main/java/com/example/assessment2/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.assessment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    //declare variables
+    EditText editCustName, editPostCode;
+    //send to next activity screen button
+    Button sendDetails;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //find customer name + suburb input view ID
+        editCustName = (EditText) findViewById(R.id.editCustName);
+        editPostCode = (EditText) findViewById(R.id.editPostCode);
+        sendDetails = (Button) findViewById(R.id.btnToNextScreen);
+
+        sendDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), cust_data.class);
+                intent.putExtra("name", editCustName.getText().toString());
+                intent.putExtra("0000", editPostCode.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+
+    //implicit intent to open web page
+    public void openAmazon(View v) {
+        String webPage = "https://www.amazon.com";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(webPage));
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/assessment2/cust_data.java
@@ -0,0 +1,55 @@
+package com.example.assessment2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class cust_data extends AppCompatActivity {
+    //receive details
+    TextView name;
+    TextView postCode;
+    TextView custArea;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cust_data);
+
+        //get details
+        name = (TextView) findViewById(R.id.getCustName);
+        postCode = (TextView) findViewById(R.id.getPostCode);
+        custArea = (TextView) findViewById(R.id.custSuburbString);
+
+        //set text on screen
+        name.setText(getIntent().getStringExtra("name"));
+        postCode.setText(getIntent().getStringExtra("0000"));
+
+        //get postCode value and convert to int
+        String postCodeValue = postCode.getText().toString();
+        int postV;
+
+        try {
+            postV = Integer.parseInt(postCodeValue.trim());
+        }
+        catch (NumberFormatException e)
+        {
+            postV = 0;
+        }
+
+        //if >2999, customer lives in VIC, if < 4000, cust does not live in VIC
+        if (postV > 2999 && postV < 4000){
+            custArea.setText(getIntent().getStringExtra("name") + " lives in VIC");
+        } else {
+            custArea.setText(getIntent().getStringExtra("name") + " does not live in VIC");
+        }
+    }
+    //previous screen activity button
+    public void previousActBtn(View view) {
+        Intent previousIntent = new Intent(cust_data.this, MainActivity.class);
+        startActivity(previousIntent);
+    }
+}
