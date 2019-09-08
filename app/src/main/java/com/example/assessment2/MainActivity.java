20d61dca67091369759ccf6e20111a2438c61f8d
Willim <54743117+Furrymeow@users.noreply.github.com>
2019-09-08 18:44:03 +1000

    Third commit

--- app/src/main/java/com/example/assessment2/MainActivity.java
+++ app/src/main/java/com/example/assessment2/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.assessment2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     //declare variables
--- app/src/main/java/com/example/assessment2/cust_data.java
+++ app/src/main/java/com/example/assessment2/cust_data.java
@@ -3,7 +3,6 @@ package com.example.assessment2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
